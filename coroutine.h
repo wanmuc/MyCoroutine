1fdbc8c9f6bcfa4008edf16fcc9740b806336ed1
wanmuc <769535441@qq.com>
2024-08-06 09:34:06 +0800

    代码调整

--- coroutine.h
+++ coroutine.h
@@ -12,18 +12,18 @@ namespace MyCoroutine {
  *  kRun->kIdle
  */
 enum class State {
-  kIdle = 1;     // 空闲
-  kReady = 2;    // 就绪
-  kRun = 3;      // 运行
-  kSuspend = 4;  // 挂起
+  kIdle = 1,    // 空闲
+  kReady = 2,   // 就绪
+  kRun = 3,     // 运行
+  kSuspend = 4, // 挂起
 };
 
 class Coroutine {
- private:
-  State state_{State::kIdle};             // 从协程当前的状态
-  std::function<void()> entry_{nullptr};  // 从协程入口函数
-  ucontext_t ctx_;                        // 从协程执行上下文
-  uint8_t* stack_{nullptr};               // 每个协程独占的协程栈，动态分配
+private:
+  State state_{State::kIdle};            // 从协程当前的状态
+  std::function<void()> entry_{nullptr}; // 从协程入口函数
+  ucontext_t ctx_;                       // 从协程执行上下文
+  uint8_t *stack_{nullptr}; // 每个协程独占的协程栈，动态分配
 };
 
-};  // namespace MyCoroutine
+}; // namespace MyCoroutine
