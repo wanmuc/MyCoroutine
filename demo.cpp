08f89caebaa4aa7a45cf2578d352d0d31e08ad5c
wanmuc <769535441@qq.com>
2024-08-06 23:32:21 +0800

    代码归档

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,28 @@
+#include <iostream>
+
 #include "schedule.h"
 
+using namespace std;
+
+MyCoroutine::Schedule schedule;
+
+void func1(int a) {
+  cout << "func1 begin" << endl;
+  cout << "func1.a=" << a << endl;
+  schedule.CoroutineYield();
+  cout << "func1 end" << endl;
+}
+
+void func2(int b) {
+  cout << "func2 begin" << endl;
+  cout << "func2.b=" << b << endl;
+  schedule.CoroutineYield();
+  cout << "func2 end" << endl;
+}
+
 int main() {
-    return 0;
+  schedule.CoroutineCreate(fun1, 10);
+  schedule.CoroutineCreate(fun2, 20);
+  schedule.Run();
+  return 0;
 }
