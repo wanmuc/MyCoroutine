9045902e9bd425429a8661f725e09be34528c445
wanmuc <769535441@qq.com>
2024-08-08 09:39:50 +0800

    代码归档

---
+++ test/mycoroutine_test.cpp
@@ -0,0 +1,28 @@
+#include "mycoroutine.h"
+
+#include <assert.h>
+
+#include <iostream>
+
+#include "UTestCore.h"
+using namespace std;
+
+void Sum(MyCoroutine::Schedule& schedule, int& total) {
+  for (int i = 0; i < 10; i++) {
+    schedule.CoroutineYield();
+    total++;
+  }
+}
+
+TEST_CASE(Schedule_Run) {
+  int total = 0;
+  MyCoroutine::Schedule schedule(10240);
+  for (int32_t i = 0; i < 10240; i++) {
+    int32_t cid = schedule.CoroutineCreate(Sum, std::ref(schedule), std::ref(total));
+    ASSERT_EQ(cid, i);
+  }
+  schedule.Run();
+  ASSERT_EQ(total, 102400);
+}
+
+RUN_ALL_TESTS();
