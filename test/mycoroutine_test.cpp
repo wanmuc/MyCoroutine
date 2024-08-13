759dd06d44653d57a37890ac50f2acdaee54a3a8
wanmuc <769535441@qq.com>
2024-08-13 13:46:32 +0800

    代码归档

--- test/mycoroutine_test.cpp
+++ test/mycoroutine_test.cpp
@@ -61,7 +61,7 @@ void WaitGroupSub(MyCoroutine::Schedule& schedule, int& total) {
 }
 
 void BatchWaitGroup(MyCoroutine::Schedule& schedule, int& total) {
-  WaitGroup wait_group(&schedule);
+  ‘MyCoroutine::WaitGroup wait_group(&schedule);
   wait_group.Add(WaitGroupSub, std::ref(schedule), std::ref(total));
   wait_group.Add(WaitGroupSub, std::ref(schedule), std::ref(total));
   wait_group.Add(WaitGroupSub, std::ref(schedule), std::ref(total));
