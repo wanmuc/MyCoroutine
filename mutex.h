039272f7fde7b990bab6ff1926fe60c536da83c0
wanmuc <769535441@qq.com>
2024-08-14 09:49:35 +0800

    代码优化

--- mutex.h
+++ mutex.h
@@ -17,4 +17,13 @@ private:
   CoMutex co_mutex_;
   Schedule *schedule_;
 };
+
+class LockGuard {
+public:
+  LockGuard(Mutex &mutex) : mutex_(mutex) { mutex_.Lock(); }
+  ~LockGuard() { mutex_.UnLock(); }
+
+private:
+  Mutex &mutex_;
+}
 }  // namespace MyCoroutine
