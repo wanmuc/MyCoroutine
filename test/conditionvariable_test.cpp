f19298ad1c94eb5c718cb524dac64fae53562860
wanmuc <769535441@qq.com>
2024-08-18 07:25:41 +0800

    add code

--- test/conditionvariable_test.cpp
+++ test/conditionvariable_test.cpp
@@ -8,44 +8,44 @@
 using namespace std;
 
 namespace {
-void CondNotifyOne(MyCoroutine::Schedule &schedule, MyCoroutine::CoCond &co_cond, list<int> &list) {
+void CondNotifyOne(MyCoroutine::Schedule &schedule, MyCoroutine::CoCond &co_cond, list<int> &queue) {
   for (int i = 0; i < 10; i++) {
     schedule.CoroutineYield();
   }
-  list.push_back(1);
+  queue.push_back(1);
   schedule.CoCondNotifyOne(co_cond);
   for (int i = 0; i < 10; i++) {
     schedule.CoroutineYield();
   }
-  list.push_back(2);
+  queue.push_back(2);
   schedule.CoCondNotifyOne(co_cond);
 }
 
-void CondWait1(MyCoroutine::Schedule &schedule, MyCoroutine::CoCond &co_cond, list<int> &list) {
-  schedule.CoCondWait(co_cond, []() { return list.size() > 0; });
-  assert(list.size() == 1);
-  assert(list.front() == 1);
-  list.pop_front();
+void CondWait1(MyCoroutine::Schedule &schedule, MyCoroutine::CoCond &co_cond, list<int> &queue) {
+  schedule.CoCondWait(co_cond, []() { return queue.size() > 0; });
+  assert(queue.size() == 1);
+  assert(queue.front() == 1);
+  queue.pop_front();
 }
 
-void CondWait2(MyCoroutine::Schedule &schedule, MyCoroutine::CoCond &co_cond, list<int> &list) {
-  schedule.CoCondWait(co_cond, []() { return list.size() > 0; });
-  assert(list.size() == 1);
-  assert(list.front() == 2);
-  list.pop_front();
+void CondWait2(MyCoroutine::Schedule &schedule, MyCoroutine::CoCond &co_cond, list<int> &queue) {
+  schedule.CoCondWait(co_cond, []() { return queue.size() > 0; });
+  assert(queue.size() == 1);
+  assert(queue.front() == 2);
+  queue.pop_front();
 }
 }  // namespace
 
 // 协程条件变量测试用例1
 TEST_CASE(CoCond_NotifyOne) {
-  list<int> list;
+  list<int> queue;
   MyCoroutine::CoCond co_cond;
   MyCoroutine::Schedule schedule(1024);
   schedule.CoCondInit(co_cond);
-  schedule.CoroutineCreate(CondNotifyOne, std::ref(schedule), std::ref(co_cond), std::ref(list));
-  schedule.CoroutineCreate(CondWait1, std::ref(schedule), std::ref(co_cond), std::ref(list));
-  schedule.CoroutineCreate(CondWait2, std::ref(schedule), std::ref(co_cond), std::ref(list));
+  schedule.CoroutineCreate(CondNotifyOne, std::ref(schedule), std::ref(co_cond), std::ref(queue));
+  schedule.CoroutineCreate(CondWait1, std::ref(schedule), std::ref(co_cond), std::ref(queue));
+  schedule.CoroutineCreate(CondWait2, std::ref(schedule), std::ref(co_cond), std::ref(queue));
   schedule.Run();
   schedule.CoCondClear(co_cond);
-  ASSERT_EQ(list.size(), 0);
+  ASSERT_EQ(queue.size(), 0);
 }
