4829c5c57db6be75b9ed708e2a0aebceb9ad7f18
wanmuc <769535441@qq.com>
2024-08-17 22:13:50 +0800

    代码规定

--- test/mycoroutine_test.cpp
+++ test/mycoroutine_test.cpp
@@ -1,13 +1,13 @@
-#include "localvariable.h"
 #include "mycoroutine.h"
-#include "waitgroup.h"
-#include "mutex.h"
 
 #include <assert.h>
 
 #include <iostream>
 
 #include "UTestCore.h"
+#include "localvariable.h"
+#include "mutex.h"
+#include "waitgroup.h"
 using namespace std;
 
 void Sum(MyCoroutine::Schedule& schedule, int& total) {
@@ -17,27 +17,41 @@ void Sum(MyCoroutine::Schedule& schedule, int& total) {
   }
 }
 
-void CoroutineLocalVariableFunc1(MyCoroutine::Schedule& schedule, MyCoroutine::CoroutineLocalVariable<int>& local_variable) {
-   local_variable.Set(100);
-   schedule.CoroutineYield();
-   assert(local_variable.Get() == 100);
+void SumBatch(MyCoroutine::Schedule& schedule, int& total) {
+  for (int i = 0; i < 10; i++) {
+    schedule.CoroutineYield();
+    total++;
+  }
 }
 
-void CoroutineLocalVariableFunc2(MyCoroutine::Schedule& schedule, MyCoroutine::CoroutineLocalVariable<int>& local_variable) {
-   local_variable.Set(200);
-   schedule.CoroutineYield();
-   assert(local_variable.Get() == 200);
+void SumHasBatch(MyCoroutine::Schedule& schedule, int& total) {
+  int32_t bid = schedule.BatchCreate();
+  schedule.BatchAdd(bid, SumBatch, std::ref(schedule), std::ref(total));
+  schedule.BatchRun(bid);
 }
 
-void CoroutineLocalVariableFunc3(MyCoroutine::Schedule& schedule, MyCoroutine::CoroutineLocalVariable<int>& local_variable) {
-   local_variable.Set(300);
-   schedule.CoroutineYield();
-   assert(local_variable.Get() == 300);
+void CoroutineLocalVariableFunc1(MyCoroutine::Schedule& schedule,
+                                 MyCoroutine::CoroutineLocalVariable<int>& local_variable) {
+  local_variable.Set(100);
+  schedule.CoroutineYield();
+  assert(local_variable.Get() == 100);
 }
 
-void BatchChild(MyCoroutine::Schedule& schedule, int& total) {
-  total++;
+void CoroutineLocalVariableFunc2(MyCoroutine::Schedule& schedule,
+                                 MyCoroutine::CoroutineLocalVariable<int>& local_variable) {
+  local_variable.Set(200);
+  schedule.CoroutineYield();
+  assert(local_variable.Get() == 200);
 }
+
+void CoroutineLocalVariableFunc3(MyCoroutine::Schedule& schedule,
+                                 MyCoroutine::CoroutineLocalVariable<int>& local_variable) {
+  local_variable.Set(300);
+  schedule.CoroutineYield();
+  assert(local_variable.Get() == 300);
+}
+
+void BatchChild(MyCoroutine::Schedule& schedule, int& total) { total++; }
 
 void BatchParent(MyCoroutine::Schedule& schedule, int& total) {
   int32_t bid = schedule.BatchCreate();
@@ -59,9 +73,7 @@ void BatchParent(MyCoroutine::Schedule& schedule, int& total) {
   schedule.BatchRun(bid);
 }
 
-void WaitGroupSub(MyCoroutine::Schedule& schedule, int& total) {
-  total++;
-}
+void WaitGroupSub(MyCoroutine::Schedule& schedule, int& total) { total++; }
 
 void BatchWaitGroup(MyCoroutine::Schedule& schedule, int& total) {
   MyCoroutine::WaitGroup wait_group(schedule);
@@ -77,8 +89,20 @@ void BatchWaitGroup(MyCoroutine::Schedule& schedule, int& total) {
   wait_group2.Wait();
 }
 
-// 协程调度的测试用例
-TEST_CASE(Schedule_Run) {
+// 协程调度的测试用例1
+TEST_CASE(Schedule_Run1) {
+  int total = 0;
+  MyCoroutine::Schedule schedule(1);
+  for (int32_t i = 0; i < 1; i++) {
+    int32_t cid = schedule.CoroutineCreate(Sum, std::ref(schedule), std::ref(total));
+    ASSERT_EQ(cid, i);
+  }
+  schedule.Run();
+  ASSERT_EQ(total, 10);
+}
+
+// 协程调度的测试用例2
+TEST_CASE(Schedule_Run2) {
   int total = 0;
   MyCoroutine::Schedule schedule(10240);
   for (int32_t i = 0; i < 10240; i++) {
@@ -87,6 +111,18 @@ TEST_CASE(Schedule_Run) {
   }
   schedule.Run();
   ASSERT_EQ(total, 102400);
+}
+
+// 协程调度的测试用例3
+TEST_CASE(Schedule_Run3) {
+  int total = 0;
+  MyCoroutine::Schedule schedule(5120, 1);
+  for (int32_t i = 0; i < 5120; i++) {
+    int32_t cid = schedule.CoroutineCreate(SumHasBatch, std::ref(schedule), std::ref(total));
+    ASSERT_EQ(cid, i);
+  }
+  schedule.Run();
+  ASSERT_EQ(total, 51200);
 }
 
 // 协程本地变量的测试用例
