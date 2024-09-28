b6572bff979d2eebd096d5db599a50ffc069f530
wanmuc <769535441@qq.com>
2024-09-28 16:11:25 +0800

    新增单元测试代码

--- test/rwlock_test.cpp
+++ test/rwlock_test.cpp
@@ -57,17 +57,17 @@ void RdLockResume4WrThenRd(MyCoroutine::Schedule &schedule, MyCoroutine::RWLock 
   assert(value == 2);
   rvalue++;
 }
-void RdLockResume4RdThenWr(MyCoroutine::Schedule &schedule, MyCoroutine::RWLock &rwlock, int &value) {
-  MyCoroutine::RdLockGuard lock_guard(rwlock);
-  assert(value == 0);
+
+void WrLockResume4RdThenWr(MyCoroutine::Schedule &schedule, MyCoroutine::RWLock &rwlock, int &value) {
+  MyCoroutine::WrLockGuard lock_guard(rwlock);
   value++;
   schedule.CoroutineYield();
   value++;
 }
-void WrLockResume4RdThenWr(MyCoroutine::Schedule &schedule, MyCoroutine::RWLock &rwlock, int &value, int &wvalue) {
-  MyCoroutine::WrLockGuard lock_guard(rwlock);
-  assert(value == 2);
-  wvalue++;
+void RdLockResume4RdThenWr(MyCoroutine::Schedule &schedule, MyCoroutine::RWLock &rwlock, int &value) {
+  MyCoroutine::RdLockGuard lock_guard(rwlock);
+  value++;
+  schedule.CoroutineYield();
 }
 }  // namespace
 
@@ -108,26 +108,35 @@ TEST_CASE(CoRWLock_Resume_WrLockThenRdLock) {
   ASSERT_EQ(rvalue, 2);
 }
 
-// 协程读写锁测试用例-Resume-多个写锁因为读锁而挂起，后面被唤醒
+// 协程读写锁测试用例-Resume-被写锁挂起的有写锁也有读锁，后面被唤醒
 TEST_CASE(CoRWLock_Resume_RdLockThenWrLock) {
   int value = 0;
-  int wvalue = 0;
   MyCoroutine::Schedule schedule(1024);
   MyCoroutine::RWLock rwlock(schedule);
+  schedule.CoroutineCreate(WrLockResume4RdThenWr, std::ref(schedule), std::ref(rwlock), std::ref(value));
   schedule.CoroutineCreate(RdLockResume4RdThenWr, std::ref(schedule), std::ref(rwlock), std::ref(value));
-  schedule.CoroutineCreate(WrLockResume4RdThenWr, std::ref(schedule), std::ref(rwlock), std::ref(value),
-                           std::ref(wvalue));
-  schedule.CoroutineCreate(WrLockResume4RdThenWr, std::ref(schedule), std::ref(rwlock), std::ref(value),
-                           std::ref(wvalue));
-  schedule.CoroutineResume(0);  // 第一次加读锁成功
+  schedule.CoroutineCreate(RdLockResume4RdThenWr, std::ref(schedule), std::ref(rwlock), std::ref(value));
+  schedule.CoroutineCreate(WrLockResume4RdThenWr, std::ref(schedule), std::ref(rwlock), std::ref(value));
+  schedule.CoroutineCreate(WrLockResume4RdThenWr, std::ref(schedule), std::ref(rwlock), std::ref(value));
+  schedule.CoroutineResume(0);  // 第一次加写锁成功
   ASSERT_EQ(value, 1);
-  schedule.CoroutineResume(1);  // 写锁被挂起
-  schedule.CoroutineResume(2);  // 写锁被挂起
-  ASSERT_EQ(wvalue, 0);
-  schedule.CoroutineResume(0);  // 读锁释放
+  schedule.CoroutineResume(1);  // 读锁被挂起
+  schedule.CoroutineResume(2);  // 读锁被挂起
+  schedule.CoroutineResume(3);  // 写锁被挂起
+  schedule.CoroutineResume(4);  // 写锁被挂起
+  ASSERT_EQ(value, 1);
+  schedule.CoroutineResume(0);  // 写锁释放
   ASSERT_EQ(value, 2);
-  schedule.CoRWLockResume();  // 唤醒一个写锁
-  ASSERT_EQ(wvalue, 1);
-  schedule.CoRWLockResume();  // 再唤醒一个写锁
-  ASSERT_EQ(wvalue, 2);
+  schedule.CoRWLockResume();  // 唤醒2个读锁
+  ASSERT_EQ(value, 4);
+  schedule.CoroutineResume(1);  // 解读锁
+  schedule.CoroutineResume(2);  // 解读锁
+  schedule.CoRWLockResume();    // 唤醒1个写锁
+  ASSERT_EQ(value, 5);
+  schedule.CoroutineResume(3);  // 解写锁
+  ASSERT_EQ(value, 6);
+  schedule.CoRWLockResume();  // 唤醒最后 1个写锁
+  ASSERT_EQ(value, 7);
+  schedule.CoroutineResume(3);  // 解写锁
+  ASSERT_EQ(value, 8);
 }
