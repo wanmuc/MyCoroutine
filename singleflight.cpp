664a9f9723b4ca93154bbc5e416aa28247823af4
wanmuc <769535441@qq.com>
2024-10-06 18:04:07 +0800

    代码优化

--- singleflight.cpp
+++ singleflight.cpp
@@ -5,22 +5,22 @@ void Schedule::CoSingleFlightInit(CoSingleFlight& single_flight) {
   single_flight.state = SingleFlightState::kInit;  // SingleFlight状态
 }
 
-void Schedule::CoSingleFlightClear(CoSingleFlight& single_flight) { single_flights_.erase(&single_flight.key); }
+void Schedule::CoSingleFlightClear(CoSingleFlight& single_flight) { single_flights_.erase(single_flight.key); }
 
 int Schedule::CoSingleFlightResume() {
   assert(is_master_);
   int count = 0;
-  for (auto* single_flight : single_flights_) {
-    if (single_flight->state != SingleFlightState::kFinish) continue;  // 没执行完，不需要唤醒其他从协程
-    if (single_flight->suspend_cid_set.size() <= 0) continue;  // 是没有挂起的从协程，也不需要唤醒
+  for (auto item : single_flights_) {
+    if (item.second->state != SingleFlightState::kFinish) continue;  // 没执行完，不需要唤醒其他从协程
+    if (item.second->suspend_cid_set.size() <= 0) continue;          // 是没有挂起的从协程，也不需要唤醒
     // 唤醒所有等待的从协程
-    for (const auto& cid : single_flight->suspend_cid_set) {
+    for (const auto& cid : item.second->suspend_cid_set) {
       CoroutineResume(cid);
       count++;
     }
-    single_flight->suspend_cid_set.clear();
+    item.second->suspend_cid_set.clear();
     // 状态自动扭转成kInit，这样相同key的SingleFlight在执行完之后，就可以再次执行（这个是和CallOnce的最大区别）
-    single_flight->state = SingleFlightState::kInit;
+    item.second->state = SingleFlightState::kInit;
   }
   return count;
 }
