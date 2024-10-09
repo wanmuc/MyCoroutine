3660ac6f05ccfde2d016ebdd29ca2c0c3d56587c
wanmuc <769535441@qq.com>
2024-10-09 08:35:11 +0800

    代码归档

--- test/singleflight_test.cpp
+++ test/singleflight_test.cpp
@@ -3,58 +3,62 @@
 #include <iostream>
 
 #include "UTestCore.h"
-#include "callonce.h"
+#include "singleflight.h"
 #include "mycoroutine.h"
 using namespace std;
 
 namespace {
 
-void CallOnceFunc(MyCoroutine::Schedule &schedule, int &value) {
-  cout << "CallOnceFunc callonce" << endl;
+void SingleFlightFunc(MyCoroutine::Schedule &schedule, int &value) {
+  cout << "SingleFlightFunc SingleFlight" << endl;
   schedule.CoroutineYield();
   value++;
-  assert(1 == value);
 }
 
-void CallOnceInit(MyCoroutine::Schedule &schedule, MyCoroutine::CallOnce &callonce, int &value) {
-  callonce.Do(CallOnceFunc, std::ref(schedule), std::ref(value));
+void SingleFlightInit(MyCoroutine::Schedule &schedule, MyCoroutine::SingleFlight &single_flight, int &value) {
+  single_flight.Do(SingleFlightFunc, std::ref(schedule), std::ref(value));
 }
-void CallOnceInCall(MyCoroutine::Schedule &schedule, MyCoroutine::CallOnce &callonce, int &value, int &incallvalue) {
-  callonce.Do(CallOnceFunc, std::ref(schedule), std::ref(value));
+void SingleFlightInCall(MyCoroutine::Schedule &schedule, MyCoroutine::SingleFlight &single_flight, int &value, int &incallvalue) {
+  single_flight.Do(SingleFlightFunc, std::ref(schedule), std::ref(value));
   incallvalue++;
 }
-void CallOnceFinish(MyCoroutine::Schedule &schedule, MyCoroutine::CallOnce &callonce, int &value, int &finishvalue) {
-  callonce.Do(CallOnceFunc, std::ref(schedule), std::ref(value));
+void SingleFlightFinish(MyCoroutine::Schedule &schedule, MyCoroutine::SingleFlight &single_flight, int &value, int &finishvalue) {
+  single_flight.Do(SingleFlightFunc, std::ref(schedule), std::ref(value));
   finishvalue++;
 }
 }  // namespace
 
-// 协程CallOnce测试用例
-TEST_CASE(CoCallOnce_ALL) {
+// 协程SingleFlight测试用例
+TEST_CASE(CoSingleFlight_ALL) {
   int value = 0;
   int incallvalue = 0;
   int finishvalue = 0;
   MyCoroutine::Schedule schedule(1024);
-  MyCoroutine::CallOnce callonce(schedule);
-  schedule.CoroutineCreate(CallOnceInit, std::ref(schedule), std::ref(callonce), std::ref(value));
-  schedule.CoroutineCreate(CallOnceInCall, std::ref(schedule), std::ref(callonce), std::ref(value),
+  MyCoroutine::SingleFlight single_flight(schedule);
+  schedule.CoroutineCreate(SingleFlightInit, std::ref(schedule), std::ref(single_flight), std::ref(value));
+  schedule.CoroutineCreate(SingleFlightInCall, std::ref(schedule), std::ref(single_flight), std::ref(value),
                            std::ref(incallvalue));
-  schedule.CoroutineCreate(CallOnceInCall, std::ref(schedule), std::ref(callonce), std::ref(value),
+  schedule.CoroutineCreate(SingleFlightInCall, std::ref(schedule), std::ref(single_flight), std::ref(value),
                            std::ref(incallvalue));
-  schedule.CoroutineCreate(CallOnceInCall, std::ref(schedule), std::ref(callonce), std::ref(value),
+  schedule.CoroutineCreate(SingleFlightInCall, std::ref(schedule), std::ref(single_flight), std::ref(value),
                            std::ref(incallvalue));
-  schedule.CoroutineCreate(CallOnceFinish, std::ref(schedule), std::ref(callonce), std::ref(value),
+  schedule.CoroutineCreate(SingleFlightFinish, std::ref(schedule), std::ref(single_flight), std::ref(value),
                            std::ref(finishvalue));
-  schedule.CoroutineResume(0);  // CallOnce状态进入kInit
+  schedule.CoroutineCreate(SingleFlightInit, std::ref(schedule), std::ref(single_flight), std::ref(value));
+  schedule.CoroutineResume(0);  // single_flight状态进入kInit
   schedule.CoroutineResume(1);  // 被挂起
   schedule.CoroutineResume(2);  // 被挂起
   schedule.CoroutineResume(3);  // 被挂起
-  ASSERT_EQ(incallvalue, 0);    // 因为CallOnceInCall都被挂起，所以incallvalue值还是0
-  schedule.CoroutineResume(0);  // CallOnce状态进入kFinish
+  ASSERT_EQ(incallvalue, 0);    // 因为SingleFlightInCall都被挂起，所以incallvalue值还是0
+  schedule.CoroutineResume(0);  // single_flight状态进入kFinish
   ASSERT_EQ(value, 1);
-  schedule.CoCallOnceResume();  // 唤醒CallOnceInCall的3个从协程
+  schedule.CoSingleFlightResume();  // 唤醒SingleFlightInCall的3个从协程
   ASSERT_EQ(incallvalue, 3);
-  schedule.CoroutineResume(4);  // 唤醒CallOnceFinish的从协程
+  schedule.CoroutineResume(4);  // 唤醒SingleFlightFinish的从协程
   ASSERT_EQ(value, 1);
   ASSERT_EQ(finishvalue, 1);
+  schedule.CoroutineResume(5);  // single_flight状态重新进入kInit
+  ASSERT_EQ(value, 1);
+  schedule.CoroutineResume(5);  // single_flight状态重新进入kFinish
+  ASSERT_EQ(value, 2);
 }
