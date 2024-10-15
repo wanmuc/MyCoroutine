0f272cd063b8c155eb37f93e8675fca293483bb7
wanmuc <769535441@qq.com>
2024-10-15 09:24:06 +0800

    新增代码

--- demo/channel/channel.cpp
+++ demo/channel/channel.cpp
@@ -30,8 +30,8 @@ int main() {
   // 创建一个协程调度对象，并自动生成大小为1024的协程池
   Schedule schedule(1024);
   Channel<int> channel(schedule, 10);
-  schedule.CoroutineCreate(Producer, std::ref(schedule), std::ref(schedule));
-  schedule.CoroutineCreate(Consumer, std::ref(schedule), std::ref(schedule));
+  schedule.CoroutineCreate(Producer, std::ref(schedule), std::ref(channel));
+  schedule.CoroutineCreate(Consumer, std::ref(schedule), std::ref(channel));
   schedule.Run();  // Run函数完成从协程的自行调度，直到所有的从协程都执行完
   return 0;
 }
